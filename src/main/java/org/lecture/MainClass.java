6f7ae8df1fef69df7d3eefc0e048939f2ef06535
Miklos Komlosy <114089706+Mikomy@users.noreply.github.com>
2024-05-16 10:33:38 +0200

    Initial commit

---
+++ src/main/java/org/lecture/MainClass.java
@@ -0,0 +1,10 @@
+package org.lecture;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Program program = new Program();
+        program.start();
+    }
+}
